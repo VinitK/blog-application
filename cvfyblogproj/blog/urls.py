e9cc19ddb9e98f5515ae557fe2b345e907bb9d2a
vinitcutshort <vinit@cutshort.io>
2019-03-11 09:03:47 +0530

    change of homepage

--- cvfyblogproj/blog/urls.py
+++ cvfyblogproj/blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.AboutView.as_view(), name='about'),
-    path('about/', views.AboutView.as_view(), name='about'),
+    path('blog/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
